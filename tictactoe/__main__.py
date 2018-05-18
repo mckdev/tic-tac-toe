dda77ee6b9eed5a3b55f54a69cec3adbd19dff00
Maciej Lorenc <lorencmaciek@gmail.com>
2018-05-18 10:09:21 +0200

    Added comments to config class.

--- tictactoe/__main__.py
+++ tictactoe/__main__.py
@@ -1,11 +1,14 @@
 from tictactoe import Game, GameConfig
 
 def main(args=None):
+    """Main program routine."""    
     print("=======================\nTic-Tac-Toe: Remastered\n=======================")
 
-    config = GameConfig('config.json')
+    config = GameConfig('config.json')  
+    # Note that config.json is exptected to be found in project root.
 
     if config.is_valid():
+        # Intialize Game instance when we are sure the config is valid.
         game = Game(config)
         game.run()
 
--- tictactoe/config.py
+++ tictactoe/config.py
@@ -6,19 +6,40 @@ from json import JSONDecodeError
 
 
 class GameConfig(UserDict):
-    """Initiates, validates and holds config variables in a dict."""
+    """Initializes, validates and holds config variables in a dict.
+
+    It expects to find the config file in project root, i.e. one level above
+    the directory with config.py.
+    
+    Args:
+        filename (string): Filename of the json file with config.
+
+    Attributes:
+        data (dict): Deserialized config.json to be used by the application.
+    
+    """
     def __init__(self, filename):
         print('Loading config from file ...')
         self.data = self.from_json(filename)
 
     def from_json(self, filename):
+        """Loads config from a json file, deserializes it and returns a dict.
+
+        It expects to find the config file one level above current directory.
+
+        Args:
+            filename (string): Filename of the json file with config.
+        """
         try:
-            project_path = os.path.abspath(os.path.dirname(__file__))
-            config_path = os.path.join(project_path, '../' + filename)
+            # Transform filename to a path one level above the current path.
+            current_path = os.path.abspath(os.path.dirname(__file__))
+            config_path = os.path.join(current_path, '../' + filename)
+            # Load the file from the created path.
             with open(config_path, 'r') as f:
                 config = json.load(f)
                 return config
         except JSONDecodeError as e:
+            # Raises JSONDecodeError when file is empty or syntactically incorrect.
             print('There was a problem with parsing your config file.',
                   'Check your config.json for errors or use the config from config-example.json.')
             return
@@ -30,6 +51,7 @@ class GameConfig(UserDict):
             sys.exit(1)
 
     def is_valid(self):
+        """Runs all validations on the config file and returns True if they pass."""
         grid_size = self.validate_grid_size(self.data)
         players = self.validate_players(self.data)
         chars = self.validate_player_chars(players)
@@ -38,6 +60,12 @@ class GameConfig(UserDict):
             return True
 
     def validate_grid_size(self, config):
+        """Validates grid.size from the config file and returns it as int if
+         it's available and valid.
+        
+        Args:
+            config (dict): The deserialized config dict from the config.json file.
+        """
         try:
             # Check if grid size can be converted to int or raise ValueError.
             grid_size = int(config['grid']['size'])
@@ -58,6 +86,12 @@ class GameConfig(UserDict):
             sys.exit(1)  
 
     def validate_players(self, config):
+        """Validates players from the config file and returns a list of
+        players dicts if they are all valid.
+
+        Args:
+           config (dict): The deserialized config dict from the config file.
+        """
         try:
             # Create a list of players from the config dictionary.
             players = [ config['players'][player] for player in config['players'] ]
@@ -71,7 +105,13 @@ class GameConfig(UserDict):
             sys.exit(1)         
 
     def validate_player_chars(self, players):
-        try:
+        """Validate players.player.char from the config file and returns a list
+        of chars if they are available, valid and unique.
+        
+        Args:
+            players (list): List of player dicts produced by validate_players.
+        """
+            try:
             # Create a list of chars from the players dictionary.
             chars = [ player['char'].strip() for player in players ]
             # Strip is used to avoid whitespaces being counted as characters.
@@ -94,6 +134,13 @@ class GameConfig(UserDict):
             sys.exit(1)
 
     def validate_ai_settings(self, players):
+        """Validates the players.player.ai settings and returns a list of them
+        or True if the setting couldn't be found.
+
+        It ignores KeyError and simply returns True if no player.ai was found
+        in the config, because the Player class can handle it later and the ai
+        to False.
+        """
         try:
             # Create a list of `ai` values from the players dictionary.
             ai_settings = [ player['ai'] for player in players ]
